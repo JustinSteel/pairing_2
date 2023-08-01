722297262926f681278a64aa44efbe9d55d13282
Justin Lee <justinslee201100@yahoo.com>
2023-08-01 14:26:48 -0600

    Chand and to or

--- plants.rb
+++ plants.rb
@@ -1,5 +1,6 @@
 gets_sunshine = true
 ounces_of_water = 7
-if gets_sunshine == true && ounces_of_water >= 10
+if gets_sunshine == true || ounces_of_water >= 10
     puts "Your plant is thriving"
 else  puts "Your plant is dead!"
+end
